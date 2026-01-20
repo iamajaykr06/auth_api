f8e0207de95d931874ee2f6386f732d98308ffa6
Ajay Kumar <iamajayk.r06@gmail.com>
2026-01-20 22:11:38 +0530

    chore: add application logging configuration

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,18 @@ from flask import Flask
 from .config import DevelopmentConfig
 from .extensions import jwt
 from .utils.errors import register_error_handlers
+from .utils.logger import setup_logging
 
 def create_app():
+
+    setup_logging()
+
     app = Flask(__name__)
     app.config.from_object(DevelopmentConfig)
 
     jwt.init_app(app)
     register_error_handlers(app)
+
     @app.route("/health")
     def health_check():
         return {"status":"ok"},200
