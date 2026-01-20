859398b589fe815374a4925a3cd02d9243a82340
Ajay Kumar <iamajayk.r06@gmail.com>
2026-01-20 21:41:18 +0530

    core: add extensions module and initialize jwt

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from .config import DevelopmentConfig
+from .extensions import jwt
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(DevelopmentConfig)
 
+    jwt.init_app(app)
     @app.route("/health")
     def health_check():
         return {"status":"ok"},200
