520638ef4d9e23df39700c7e2760e2abbb168bab
Ajay Kumar <iamajayk.r06@gmail.com>
2026-01-21 10:17:59 +0530

    service: add user creation business logic

---
+++ app/services/user_service.py
@@ -0,0 +1,14 @@
+from app.models.user import UserModel
+from app.utils.security import hash_password
+from app.utils.errors import APIError
+
+class UserService:
+
+    @staticmethod
+    def create_user(email, password):
+        existing_user = UserModel.find_by_email(email)
+        if existing_user:
+            raise APIError("User already exists", 409)
+
+        password_hash = hash_password(password)
+        UserModel.create(email, password_hash)
