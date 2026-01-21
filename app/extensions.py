4532afb43f562b4e6374aba1dc8a4ab1d75ee424
Ajay Kumar <iamajayk.r06@gmail.com>
2026-01-21 09:46:17 +0530

    db: add mysql connection helper

--- app/extensions.py
+++ app/extensions.py
@@ -1,2 +1,13 @@
+import mysql.connector
 from flask_jwt_extended import JWTManager
+from flask import current_app
+
 jwt = JWTManager()
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host = current_app.config["DB_HOST"],
+        user = current_app.config["DB_USER"],
+        password = current_app.config["DB_PASSWORD"],
+        name = current_app.config["DB_NAME"]
+    )
